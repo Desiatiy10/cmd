94cca35ba87b4748c46f9e5069ebbde85574d922
Anton M. <ton190320000@yandex.ru>
2025-06-11 21:33:43 +0300

    Слайс с днями недели

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Homework №2")
-	fmt.Println("Работа с Git")
+	week := []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
+	weekend := week[5:7]
+	fmt.Println("Выходные: ", weekend)
+	week = week[:5]
+	fmt.Println("Рабочие дни: ", week)
+
+	week = append(week, weekend...)
+	fmt.Println("Полная неделя: ", week)
 }
