4cc7641180902aee8c6ac4ab96ec444148e21e48
Anton M. <ton190320000@yandex.ru>
2025-06-12 13:38:49 +0300

    Работа с мапами

--- main.go
+++ main.go
@@ -3,12 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	week := []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}
-	weekend := week[5:7]
-	fmt.Println("Выходные: ", weekend)
-	week = week[:5]
-	fmt.Println("Рабочие дни: ", week)
+	accountingBooks := map[string]map[string][]string{
+		"Anton M.": {
+			"books":     {"Metro 2033", "Chack Palanick", "Computer Science"},
+			"newspaper": {},
+		},
+		"Sergey L.": {
+			"books":     {},
+			"newspaper": {"WP", "RT", "some trash"},
+		},
+		"Mikhalil K.": {},
+	}
 
-	week = append(week, weekend...)
-	fmt.Println("Полная неделя: ", week)
+	count := 0
+
+	for _, categories := range accountingBooks {
+		hasBookOrPaper := false
+
+		for categori, items := range categories {
+			if categori != "" || len(items) > 0 {
+				hasBookOrPaper = true
+				break
+			}
+		}
+		if hasBookOrPaper {
+			count++
+		}
+	}
+	
+	fmt.Printf("Кол-во читателей с изданиями на руках: %d\n ", count)
+
+	for reader, categories := range accountingBooks {
+		total := 0
+
+		for _, items := range categories {
+			total += len(items)
+		}
+		fmt.Printf("Читатель: %s Колличество изданий на руках: %d\n", reader, total)
+	}
 }
